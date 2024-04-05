bd6b118aae1c966ebe661076f353958951aa34a3
Amarendra <Amarendras1@adapthealth.com>
2024-04-05 15:32:25 +0530

    updated test

--- cypress/e2e/TDD/ApplyLeaveTest.cy.js
+++ cypress/e2e/TDD/ApplyLeaveTest.cy.js
@@ -1,58 +1,58 @@
-import LoginPage from '../../pages/loginPage'
-import HomePage from '../../pages/homePage'
-import LeavePage from '../../pages/leavePage'
+// import LoginPage from '../../pages/loginPage'
+// import HomePage from '../../pages/homePage'
+// import LeavePage from '../../pages/leavePage'
 
-describe('Add Employee Test Cases', () => {
+// describe('Add Employee Test Cases', () => {
 
-    const loginPage = new LoginPage();
-    const homePage = new HomePage();
-    const leavePage = new LeavePage();
+//     const loginPage = new LoginPage();
+//     const homePage = new HomePage();
+//     const leavePage = new LeavePage();
 
-    before(function () {
-        cy.fixture('loginData').then(function (login) {
-            globalThis.login = login;
-        })
-        cy.fixture('applyLeaveData').then(function (leaveData) {
-            globalThis.leaveData = leaveData;
-        })
-    })
+//     before(function () {
+//         cy.fixture('loginData').then(function (login) {
+//             globalThis.login = login;
+//         })
+//         cy.fixture('applyLeaveData').then(function (leaveData) {
+//             globalThis.leaveData = leaveData;
+//         })
+//     })
 
-    beforeEach(function () {
-        cy.openUrl('/')
-        loginPage.doLogin(login.validCredentials.username, login.validCredentials.password)
-        homePage.getTitle().then(function (title) {
-            assert.equal(login.validCredentials.title, title)
-        })
-    })
+//     beforeEach(function () {
+//         cy.openUrl('/')
+//         loginPage.doLogin(login.validCredentials.username, login.validCredentials.password)
+//         homePage.getTitle().then(function (title) {
+//             assert.equal(login.validCredentials.title, title)
+//         })
+//     })
 
-    it.only('verify user is able to add new Employee', () => {
-        homePage.selectApplyLeave()
-        leavePage.getApplyLeaveValue().then(function (msg) {
-            assert.equal(leaveData.message, msg)
-        })
-    })
+//     it.only('verify user is able to add new Employee', () => {
+//         homePage.selectApplyLeave()
+//         leavePage.getApplyLeaveValue().then(function (msg) {
+//             assert.equal(leaveData.message, msg)
+//         })
+//     })
 
-})
+// })
 
-// visit the libary page
-cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
+// // visit the libary page
+// cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
 
-cy.intercept({
-    method: 'GET',
-    url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
-},
-    {
-        statusCode: 200,
-        body: [{
-            "book_name": "RestAssured with Java",
-            "isbn": "RSU",
-            "aisle": "2301"
-        }]
-    }).as('bookretrievals');
+// cy.intercept({
+//     method: 'GET',
+//     url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+// },
+//     {
+//         statusCode: 200,
+//         body: [{
+//             "book_name": "RestAssured with Java",
+//             "isbn": "RSU",
+//             "aisle": "2301"
+//         }]
+//     }).as('bookretrievals');
 
-cy.get("button[class='btn btn-primary']").click()
+// cy.get("button[class='btn btn-primary']").click()
 
-cy.get('p').should('have.text', 'Sorry we have only one book available')
+// cy.get('p').should('have.text', 'Sorry we have only one book available')
 
 
 
